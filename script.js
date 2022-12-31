a768393bca95c2d02960a4984266046a7093a4e8
ArshvirSk <arshvirsk26@gmail.com>
2023-01-01 03:18:14 +0530

    Dark mode enabled

--- script.js
+++ script.js
@@ -1,38 +1,60 @@
 let weather = {
-    "apiKey":"bb1ae91edbd801c0a95f4a93b14f7a71",
-    fetchWeather: function(city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
-            .then((response) => response.json())
-            .then((data) => this.displayWeather(data));
-    },
-    displayWeather: function(data) {
-        const { name } = data;
-        const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
-        const { speed } = data.wind;
-        // console.log(name, icon, description, temp, humidity, speed);
-        document.querySelector(".city").innerHTML = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".description").innerHTML = description;
-        document.querySelector(".temp").innerHTML = temp + "&#176;C";
-        document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerHTML = "Wind Speed: " + speed + "km/h";
-        document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+ name +"')";
-    },
-    search: function () {
-        this.fetchWeather(document.querySelector(".search-bar").value);
-    }
+  apiKey: "bb1ae91edbd801c0a95f4a93b14f7a71",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    // console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerHTML = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerHTML = description;
+    document.querySelector(".temp").innerHTML = temp + "&#176;C";
+    document.querySelector(".humidity").innerHTML =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerHTML = "Wind Speed: " + speed + "km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
 };
 
-document.querySelector(".search button").addEventListener("click", function() {
-    weather.search();
+function darkToggle() {
+    console.log("Toggle");
+    var card = document.querySelector(".card");
+    var search = document.querySelector(".search-bar");
+    var button = document.querySelector(".srchbutton");
+    var button1 = document.querySelector(".togglebtn");
+    card.classList.toggle("dark");
+    search.classList.toggle("dark");
+    button.classList.toggle("dark");
+    button1.classList.toggle("dark");
+}
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
 });
 
-document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
-        weather.search();
+      weather.search();
     }
-});
+  });
 
 weather.fetchWeather("Mumbai");
